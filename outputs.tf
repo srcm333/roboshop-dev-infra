c0c488b561163fffc138cd881a3370779ca69bb7
srcm333 <srcm333@gmail.com>
2026-06-26 00:54:37 +0530

    terraform

--- outputs.tf
+++ outputs.tf
@@ -3,13 +3,13 @@ output "vpu_id" {
 }
 
 output "public_subnet_ids" {
-    value = module.vpc.public_subnet_id
+    value = module.vpc.public_subnet_ids
 }
 
 output "private_subnet_ids" {
-    value = module.vpc.private_subnet_id
+    value = module.vpc.private_subnet_ids
 }
 
 output "database_subnet_ids" {
-    value = module.vpc.database_subnet_id
+    value = module.vpc.database_subnet_ids
 }
