1c3dd7d2e013796448a3acc4e26b9679e9a15bf3
srcm333 <srcm333@gmail.com>
2026-06-26 20:59:58 +0530

    terraform

--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -4,3 +4,24 @@ resource "aws_ssm_parameter" "vpc_id" {
   value = module.vpc.vpc_id
   overwrite = true
 }
+
+resource "aws_ssm_parameter" "public_subnet_ids" {
+  name  = "/${var.project}/${var.environment}/public_subnet_ids"
+  type  = "String"
+  value = join(",", module.vpc.public_subnet_ids)
+  overwrite = true
+}
+
+resource "aws_ssm_parameter" "private_subnet_ids" {
+  name  = "/${var.project}/${var.environment}/private_subnet_ids"
+  type  = "String"
+  value = join(",", module.vpc.private_subnet_ids)
+  overwrite = true
+}
+
+resource "aws_ssm_parameter" "database_subnet_ids" {
+  name  = "/${var.project}/${var.environment}/database_subnet_ids"
+  type  = "String"
+  value = join(",", module.vpc.database_subnet_ids)
+  overwrite = true
+}
