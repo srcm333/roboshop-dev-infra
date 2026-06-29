2d6827a527e24b54d44d878278c6a9d5a2386bc3
srcm333 <srcm333@gmail.com>
2026-06-30 01:42:19 +0530

    ansible

---
+++ 50-backend-alb/variables.tf
@@ -0,0 +1,16 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z07343973IZWQHPNL7C9J"
+}  
+
+variable "domain_name" {
+    default = "devpreactice.online"
+}
+
