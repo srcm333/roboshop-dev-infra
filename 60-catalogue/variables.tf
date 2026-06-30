10224e322a5ba781d9d62d5992cbaa26a7dada59
srcm333 <srcm333@gmail.com>
2026-06-30 21:07:51 +0530

    ansible

---
+++ 60-catalogue/variables.tf
@@ -0,0 +1,20 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "app_version" {
+    default = "v3"
+}
+
+#variable "zone_id" {
+#    default = "Z07343973IZWQHPNL7C9J"
+#}  
+#
+#variable "domain_name" {
+#    default = "devpreactice.online"
+#}
+
