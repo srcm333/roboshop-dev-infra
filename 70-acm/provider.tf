fb24bfc20ddc512f392bb9fa68ecf80b3cf36e02
srcm333 <srcm333@gmail.com>
2026-07-01 23:25:19 +0530

    terraform

--- 70-acm/provider.tf
+++ 70-acm/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   # remote state storage location
   backend "s3" {
-    bucket         = "remote-state-90s-dev"
+    bucket         = "remote-statefile-90s-dev"
     key            = "roboshop-acm.tfstate"
     region         = "us-east-1"
     encrypt        = true
