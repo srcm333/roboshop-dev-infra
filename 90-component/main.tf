760a22dcef1c6f47997bb838eeaa9cf727cb0c32
srcm333 <srcm333@gmail.com>
2026-07-02 21:41:44 +0530

    terraform

--- 90-component/main.tf
+++ 90-component/main.tf
@@ -1,8 +1,8 @@
 module "components" {
     for_each = var.components
-    source = "../../terraform-roboshop-component"
+    source = "https://github.com/srcm333/terraform-roboshop-component.git"
     environment = var.environment
     component = each.key
     app_version = each.value.app_version
     rule_priority = each.value.rule_priority
-}
+} 
