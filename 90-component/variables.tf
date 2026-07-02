e1c0f49e200ef300a04d0bbf5e41e5f2dac5752d
srcm333 <srcm333@gmail.com>
2026-07-02 23:02:11 +0530

    terraform

--- 90-component/variables.tf
+++ 90-component/variables.tf
@@ -20,6 +20,22 @@ variable "components" {
             rule_priority = 10
             app_version = "v3"
         }
+        user = {
+            rule_priority = 20
+            app_version = "v3"
+        }
+        cart = {
+            rule_priority = 30
+            app_version = "v3"
+        }
+        shipping = {
+            rule_priority = 40
+            app_version = "v3"
+        }
+        payment = {
+            rule_priority = 50
+            app_version = "v3"
+        }
         
     }
 }
